c1d5bb41b86b2556e278495bbc8278b79a1e575b
hrfmartins <32126871+hrfmartins@users.noreply.github.com>
2020-12-19 14:15:11 +0000

    Ads filtering using RegEx

--- libgen_API/search_request.py
+++ libgen_API/search_request.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
 
 # WHY
 # this SearchRequest module is used to contain all of the internal logic that end users will not need to use. 
@@ -8,48 +10,59 @@ from bs4 import BeautifulSoup
 # USAGE
 # req = search_request.SearchRequest("[QUERY]", search_type="[title]")
 
+
+def ads_cleanup(data):
+    new = []
+    for el in data:
+        if not re.match(".*ad.*", el["ID"]):
+            new.append(el)
+    return new
+
+
 class SearchRequest:
+    col_names = ["ID", "Author", "Title", "Publisher", "Year", "Pages", "Language", "Size", "Extension",
+                 "Mirror_1", "Mirror_2", "Mirror_3", "Mirror_4", "Mirror_5", "Edit"]
 
-	col_names = ["ID", "Author", "Title", "Publisher", "Year", "Pages", "Language", "Size", "Extension", 
-            "Mirror_1", "Mirror_2", "Mirror_3", "Mirror_4", "Mirror_5", "Edit"]
+    def __init__(self, query, search_type="title"):
+        self.query = query
+        self.search_type = search_type
 
-	def __init__(self, query, search_type="title"):
-		self.query = query
-		self.search_type = search_type
+    def strip_i_tag_from_soup(self, soup):
+        subheadings = soup.find_all("i")
+        for subheading in subheadings:
+            subheading.decompose()
 
-	def strip_i_tag_from_soup(self, soup):
-		subheadings = soup.find_all("i")
-		for subheading in subheadings:
-			subheading.decompose()
+    def get_search_page(self):
+        search_url = ''
+        query_parsed = "%20".join(self.query.split(" "))
+        if self.search_type.lower() == 'title':
+            search_url = f'http://libgen.li//search.php?req={query_parsed}&column=title'
+        elif self.search_type.lower() == 'author':
+            search_url = f'http://libgen.li//search.php?req={query_parsed}&column=author'
+        search_page = requests.get(search_url)
+        return search_page
 
-	def get_search_page(self):
-		query_parsed = "%20".join(self.query.split(" "))
-		if self.search_type.lower() == 'title':
-			search_url = f'http://libgen.li//search.php?req={query_parsed}&column=title'
-		elif self.search_type.lower() == 'author':
-			search_url = f'http://libgen.li//search.php?req={query_parsed}&column=author'
-		search_page = requests.get(search_url)
-		return search_page
+    def aggregate_request_data(self):
+        search_page = self.get_search_page()
+        soup = BeautifulSoup(search_page.text, 'lxml')
+        self.strip_i_tag_from_soup(soup)
 
-	def aggregate_request_data(self):
-		search_page = self.get_search_page()
-		soup = BeautifulSoup(search_page.text, 'lxml')
-		self.strip_i_tag_from_soup(soup)
+        # Libgen results contain 3 tables
+        # Table2: Table of data to scrape.
+        information_table = soup.find_all('table')[2]
 
-		# Libgen results contain 3 tables 
-		# Table2: Table of data to scrape.
-		information_table = soup.find_all('table')[2]
+        # Determines whether the link url (for the mirror) or link text (for the title) should be preserved. Both the
+        # book title and mirror links have a "title" attribute, but only the mirror links have it filled. (title vs
+        # title="libgen.io")
+        raw_data = [
+            [
+                td.a['href'] if td.find('a') and td.find('a').has_attr("title") and td.find('a')["title"] != ""
+                else ''.join(td.stripped_strings)
+                for td in row.find_all("td")
+            ]
+            for row in information_table.find_all("tr")[1:]  # Skip row 0 as it is the headings row
+        ]
 
-		# Determines whether the link url (for the mirror) or link text (for the title) should be preserved. 
-		# Both the book title and mirror links have a "title" attribute, but only the mirror links have it filled. (title vs title="libgen.io")
-		raw_data = [
-			[
-				td.a['href'] if td.find('a') and td.find('a').has_attr("title") and td.find('a')["title"] is not "" 
-				else ''.join(td.stripped_strings)
-				for td in row.find_all("td")
-			]
-			for row in information_table.find_all("tr")[1:] # Skip row 0 as it is the headings row
-		]
-
-		output_data = [ dict(zip(self.col_names, row))  for row in raw_data ]
-		return output_data
+        output_data = [dict(zip(self.col_names, row)) for row in raw_data]
+        output_data = ads_cleanup(output_data)
+        return output_data
